79050748d491e99230e22a44d52833967fe3c055
Tavinho0710 <gustavo-niehues@hotmail.com>
2019-05-25 20:21:58 -0300

    Primeira versão máquina funcionando

--- Machine/turingmachine.py
+++ Machine/turingmachine.py
@@ -11,22 +11,27 @@ class TuringMachine(object):
 		self.instrucoes = instrucoes
 		self.leitura_cabeca = self.fita[0]
 		while self.estado_atual!='END':
-			chave = (self.estado_atual, self.leitura_cabeca)
-			if chave in instrucoes:
-				instrucao = instrucoes[chave]
-				self.estado_atual = instrucao[0]
-				self.fita[self.posicao_cabeca] = instrucao[1]
-				if instrucao[2] == 'D':
-					self.posicao_cabeca += 1
-				elif instrucao[2] == 'E' and self.posicao_cabeca > 0:
-					self.posicao_cabeca -= 1
-				else:
-					pass
-				if self.posicao_cabeca == len(self.fita):
-					self.fita.append(' ')
-				self.leitura_cabeca = self.fita[self.posicao_cabeca]
-			print(''.join(self.fita))
+			self.operacao()
 		return ''.join(self.fita)
 
 	def stringToList(self, fita):
 		self.fita = list(fita)
+
+	def operacao(self):
+		chave = (self.estado_atual, self.leitura_cabeca)
+		if chave in self.instrucoes:
+			instrucao = self.instrucoes[chave]
+			self.estado_atual = instrucao[0]
+			self.fita[self.posicao_cabeca] = instrucao[1]
+			if instrucao[2] == 'D':
+				self.posicao_cabeca += 1
+			elif instrucao[2] == 'E' and self.posicao_cabeca > 0:
+				self.posicao_cabeca -= 1
+			else:
+				pass
+			if self.posicao_cabeca == len(self.fita):
+				self.fita.append(' ')
+			self.leitura_cabeca = self.fita[self.posicao_cabeca]
+		else:
+			raise Exception('A operação {0} não foi encontrada na lista de instruções'.format(chave))
+		print(''.join(self.fita))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import soma
+import instrucoes
 from Machine.turingmachine import TuringMachine
 
 fita = input("Insira fita aqui")
 
 maquina = TuringMachine()
-resultado = maquina.start(fita, soma.instrucoes)
+resultado = maquina.start(fita, instrucoes.multiplicacao)
 print(resultado)
