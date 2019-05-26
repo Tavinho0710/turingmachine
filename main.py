3a6e5d604d7349eb851937b24ab16b5dd938bab8
Gustavo Niehues <gustavo-niehues@hotmail.com>
2019-05-26 11:05:16 -0300

    Inicio interface gráfica

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
+from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QLineEdit,QVBoxLayout, QHBoxLayout
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
 
-import instrucoes
-
+qt_app = QApplication(sys.argv)
 
 class Window(QWidget):
-	def __init__(self, parent=None):
-		super(Window, self).__init__(parent)
+	def __init__(self):
+		QWidget.__init__(self)
 		self.setWindowTitle('Turing Machine')
+		self.setMinimumHeight(200)
+		self.setMinimumWidth(200)
 
-	def iniciar(self):
-		fita = self.fita.get()
-		resultado = self.maquina.start(fita, instrucoes.multiplicacao)
+		texto_1 = QLabel('Insira a fita aqui:',self)
+		entrada_fita = QLineEdit(self)
+		texto_2 = QLabel('Insira aqui a lista de simbolos separados por vírgula:')
+		entrada_simbolos = QLineEdit(self)
+		layout_1 = QHBoxLayout()
+		layout_1.addWidget(texto_1)
+		layout_1.addWidget(entrada_fita)
+		layout_1.
+		layout_2 = QHBoxLayout()
+		layout_2.addWidget(texto_2)
+		layout_2.addWidget(entrada_simbolos)
+		layout = QVBoxLayout()
+		layout.addLayout(layout_1)
+		layout.addLayout(layout_2)
+		self.setLayout(layout)
 
+	def run(self):
+		self.show()
+		qt_app.exec_()
 
-root = QApplication(sys.argv)
-app = Window()
-app.show()
-sys.exit(root.exec_())
+Window().run()
--- turingmachine.py
+++ turingmachine.py
@@ -1,42 +1,47 @@
+
+
 class TuringMachine(object):
-	def __init__(self):
-		pass
-	def start(self, fita, instrucoes):
-		self.limparMaquina()
-		self.stringToList(fita)
-		self.instrucoes = instrucoes
-		self.leitura_cabeca = self.fita[0]
-		while self.estado_atual!='END':
-			fita = self.operacao()
-		while self.fita[0] == ' ':
-			self.fita.pop(0)
-		return ''.join(self.fita)
+    def __init__(self):
+        self.fita = None
+        self.instrucoes = None
+        self.leitura_cabeca = None
 
-	def stringToList(self, fita):
-		self.fita = list(fita)
+    def start(self, fita, instrucoes):
+        self.limparMaquina()
+        self.stringToList(fita)
+        self.instrucoes = instrucoes
+        self.leitura_cabeca = self.fita[0]
+		while self.estado_atual != 'END':
+            fita = self.operacao()
+        while self.fita[0] == ' ':
+            self.fita.pop(0)
+        return ''.join(self.fita)
 
-	def operacao(self):
-		chave = (self.estado_atual, self.leitura_cabeca)
-		if chave in self.instrucoes:
-			instrucao = self.instrucoes[chave]
-			self.estado_atual = instrucao[0]
-			self.fita[self.posicao_cabeca] = instrucao[1]
-			if instrucao[2] == 'D':
-				self.posicao_cabeca += 1
-			elif instrucao[2] == 'E' and self.posicao_cabeca > 0:
-				self.posicao_cabeca -= 1
-			else:
-				pass
-			if self.posicao_cabeca == len(self.fita):
-				self.fita.append(' ')
-			self.leitura_cabeca = self.fita[self.posicao_cabeca]
-		else:
-			raise Exception('A operação {0} não foi encontrada na lista de instruções'.format(chave))
-		return self.fita
+    def stringToList(self, fita):
+        self.fita = list(fita)
 
-	def limparMaquina(self):
-		self.fita = None
-		self.posicao_cabeca = 0
-		self.instrucoes = None
-		self.estado_atual = 'q0'
-		self.leitura_cabeca = 0
+    def operacao(self):
+        chave = (self.estado_atual, self.leitura_cabeca)
+        if chave in self.instrucoes:
+            instrucao = self.instrucoes[chave]
+            self.estado_atual = instrucao[0]
+            self.fita[self.posicao_cabeca] = instrucao[1]
+            if instrucao[2] == 'D':
+                self.posicao_cabeca += 1
+            elif instrucao[2] == 'E' and self.posicao_cabeca > 0:
+                self.posicao_cabeca -= 1
+            else:
+                pass
+            if self.posicao_cabeca == len(self.fita):
+                self.fita.append(' ')
+            self.leitura_cabeca = self.fita[self.posicao_cabeca]
+        else:
+            raise Exception('A operação {0} não foi encontrada na lista de instruções'.format(chave))
+        return self.fita
+
+    def limparMaquina(self):
+        self.fita = None
+        self.posicao_cabeca = 0
+        self.instrucoes = None
+        self.estado_atual = 'q0'
+        self.leitura_cabeca = 0
